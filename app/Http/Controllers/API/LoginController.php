d8035457d9f919fb1dd1371d986aded6412585d5
Mukesh Patel <mukesh_p004@yahoo.com>
2020-06-06 15:08:01 +0000

    initial commit

---
+++ app/Http/Controllers/API/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    protected function guard()
+    {
+        return Auth::guard('guard-name');
+    }
+
+    function authenticated(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required'
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response([
+                'email' => ['The provided credentials are incorrect.'],
+            ], 404);
+        }
+    
+        return response([
+            'user' => $user,
+            'token' => $user->createToken($request->device_name)->plainTextToken
+        ], 200);
+    }
+}
